610f1e7e62845f49f11bbe8801b5b901cafcd74a
mayufo <mayufo@aliyun.com>
2018-04-09 15:51:03 +0800

    移动端 index.html menu

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@
             this.bindEvent()
         },
         bindEvent () {
+            this.menuInit()
             this.slideInit()
             this.featureInit()
         },
@@ -19,13 +20,30 @@
             });
         },
         featureInit () {
-            $('.feature li').on('mouseenter', function (e) {
+            $('.feature li').on('mouseenter', (e) => {
                 $(e.currentTarget).addClass('active')
             })
-            $('.feature li').on('mouseleave', function (e) {
+            $('.feature li').on('mouseleave', (e) => {
                 $(e.currentTarget).removeClass('active')
             })
         },
+        menuInit () {
+            // 当有子菜单的时候显示icon
+            $('.menu-list > li').has('.child-menu').find('.icon-arrow-down').show()
+
+            $('.icon-menu').on('click', () => {
+                $('.nav-menu').show();
+            })
+            $('.close').on('click', () => {
+                $('.nav-menu').hide();
+            })
+            $('.menu-list > li').on('click', (e) => {
+                if ($(e.currentTarget).find('.child-menu').length > 0) {
+                    $(e.currentTarget).find('a').toggleClass('active')
+                    $(e.currentTarget).find('.child-menu').toggle();
+                }
+            })
+        }
 
     }
     controll.init()
